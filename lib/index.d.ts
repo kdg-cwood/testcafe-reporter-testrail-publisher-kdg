8eee14c8f362c6f38675c4492c724edf8738a6e5
Adil Ben Moussa <adil.benmoussa@gmail.com>
2022-01-14 16:42:53 +0100

    feat: fix skipped status issue

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,8 @@
-declare const TestrailApi: any;
-declare function logWithFormat(text: any, colorHandler: any): void;
+export declare enum TestCaseStatus {
+    Unknown = 0,
+    Passed = 1,
+    Blocked = 2,
+    Untested = 3,
+    Retest = 4,
+    Failed = 5
+}
